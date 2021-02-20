c9c52621787c1a105b1233b19529477df0705c88
West <accounts@hoof.dev>
2021-02-19 22:34:15 -0500

    Added prettier into package.json for standard code formatting

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.json()) // JSON extraction (body-parsing)
-app.use(express.static(__dirname + '/public')) // setup static folder
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json()); // JSON extraction (body-parsing)
+app.use(express.static(__dirname + '/public')); // setup static folder
 
 app.listen(port, () => {
-	console.log('Listening on *:3000')
+  console.log('Listening on *:3000');
 });
