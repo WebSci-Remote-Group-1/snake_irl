2536cff834cf1e4177b67400126ecdbe565095f0
West <accounts@hoof.dev>
2021-02-23 11:29:56 -0500

    Add support for sass stylesheets with automatic compliation

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
 const app = express();
 const port = 3000;
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'assets/sass'),
+    dest: path.join(__dirname, 'public/assets'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/assets',
+  })
+);
 app.use(express.json()); // JSON extraction (body-parsing)
-app.use(express.static(__dirname + '/public')); // setup static folder
+app.use(express.static(path.join(__dirname, 'public'))); // setup static folder
 
 app.listen(port, () => {
   console.log('Listening on *:3000');
