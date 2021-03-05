9eddea014ae468c06cd72224fad6ab99962cb558
West <accounts@hoof.dev>
2021-03-05 18:45:51 -0500

    Add materialUI and begin work on create map page

--- src/src/components/Create.js
+++ src/src/components/Create.js
@@ -1,11 +1,30 @@
+// Import React, tools and helpers
 import React, { useState } from 'react';
 
-const Create = () => (
-  <>
-    <main>
-      <h3>Create a map</h3>
-    </main>
-  </>
-);
+// Import MaterialUI elements
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
-export default Create;
+// Import homebrewed files
+import '@assets/style/create.scss';
+
+export default function Create() {
+  const [click, setClick] = useState(false);
+
+  return (
+    <>
+      <Grid id="create-space" container justify="center" alignItems="center">
+        <Grid item>
+          <Button
+            disableElevation
+            color={click ? 'primary' : 'secondary'}
+            variant="contained"
+            onClick={() => setClick(!click)}
+          >
+            Create a map
+          </Button>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
