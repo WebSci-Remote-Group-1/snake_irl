a6d1f37c4f08bce36f316bd322a54b18e1cb729e
West <accounts@hoof.dev>
2021-03-05 15:36:01 -0500

    Add react router for page switching, react-app-rewired for path defintions, make placeholder Create and Index components for router testing

---
+++ src/src/components/Create.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Create = () => (
+  <div>
+    <h1>THIS IS THE CREATE PAGE</h1>
+  </div>
+);
+
+export default Create;
---
+++ src/src/components/TempIndex.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <h1>THIS IS A PLACEHOLDER INDEX FOR REACT ROUTER TO BE COOL WITH IT</h1>
+  </div>
+);
+
+export default Index;
