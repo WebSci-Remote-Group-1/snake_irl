08bf8fdc632a56e6abfd23db9769dfde9d68f81f
Brooke Rose Baer <brooke@chromelia.dev>
2021-03-10 18:59:53 -0500

    Add Snake_IRL logo + add to mobile index

--- src/src/components/MobileIndex.js
+++ src/src/components/MobileIndex.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import snakeirl from '@assets/img/512px-snake-irl-transparent.png';
 
 const MobileIndex = () => (
   <div>
-    <h1>Snake_IRL Mobile Index Page!</h1>
+    <img src={snakeirl} width="100%"></img>
   </div>
 );
 
