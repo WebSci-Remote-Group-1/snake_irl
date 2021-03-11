ae657f2aa4fb2eeaedd5e2e2313c5fa27cf251ea
West <accounts@hoof.dev>
2021-03-11 10:37:07 -0500

    Create dummy JSON data for maps, create theme override settings

--- src/src/components/Create.js
+++ src/src/components/Create.js
@@ -3,28 +3,52 @@ import React, { useState } from 'react';
 
 // Import MaterialUI elements
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 // Import homebrewed files
 import '@assets/style/create.scss';
+import Header from '@components/shared/Header';
+
+// Import frontend demo placeholder files
+import Maps from '@assets/frontend-placeholders/created_maps.json';
+
+const MapCard = (props) => (
+  <Card>
+    <CardContent>
+      <Typography>{props.title}</Typography>
+      <Typography>{props.description}</Typography>
+    </CardContent>
+  </Card>
+);
 
 export default function Create() {
   const [click, setClick] = useState(false);
 
+  Maps['maps'].map((map) => console.log(map));
   return (
     <>
-      <Grid id="create-space" container justify="center" alignItems="center">
-        <Grid item>
-          <Button
-            disableElevation
-            color={click ? 'primary' : 'secondary'}
-            variant="contained"
-            onClick={() => setClick(!click)}
-          >
-            Create a map
-          </Button>
+      <Header />
+      <Container>
+        <Button
+          disableElevation
+          color={click ? 'primary' : 'secondary'}
+          variant="contained"
+          onClick={() => setClick(!click)}
+        >
+          Create a map
+        </Button>
+        <Grid container justify="center" alignItems="center" spacing={5}>
+          {Maps['maps'].map((map) => (
+            <Grid item>
+              <MapCard title={map.title} description={map.description} />
+            </Grid>
+          ))}
         </Grid>
-      </Grid>
+      </Container>
     </>
   );
 }
