e22108bd13b7f35090234bb0ce7fe869cdc887f9
West <accounts@hoof.dev>
2021-04-08 00:05:58 -0400

    Working on backend structure

--- src/server.js
+++ src/server.js
@@ -1,29 +1,39 @@
+/*
+ * Core functions of the snake_irl API
+ */
+
+// Package imports
 const express = require('express');
 const { MongoClient, ObjectID } = require('mongodb');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 
+// Homebrew imports
+const MGDB_PlayerInterface = require('./lib/player_lib');
+
+// Globals
 const app = express();
 const port = 3001;
 const api_path = '/api/v1';
 
-const constructProperMongoURI = (env_var_name) => {
-  let uri =
-    'mongodb+srv://' + process.env[env_var_name] + '@' + process.env.MONGO_URI;
-
-  return uri;
-};
-
 app.use(express.json());
 
+/*
+ * =======================================
+ * Functions
+ * =======================================
+ */
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-  console.log(constructProperMongoURI('MGDB_PLAYERMANAGER'));
+
+  MGDB_PlayerInterface.createUser(null, null);
 });
 
+// Example test endpoint
 app.get(api_path + '/hello', (req, res) => {
   res.json({ status: 200, message: 'Hello world!' });
 });
 
+// Fetch player account information
 app.get(api_path + '/player/:id', (req, res) => {
   console.log(req.params.id);
   res.json({ status: 200, message: 'You are pinging the players endpoint' });
