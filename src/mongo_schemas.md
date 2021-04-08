02de02048b792e6674d27df1e1d03f151bb08d83
West <accounts@hoof.dev>
2021-04-07 23:27:30 -0400

    Add mongo_schemas.md for database organization and record keeping'

---
+++ src/mongo_schemas.md
@@ -0,0 +1,8 @@
+# Here is a definition of Snake_IRL's database schemas for MongoDB
+
+## MongoDB Users
+- `playerManager`: [`player_accounts`] --> Responsible for managing player account information
+
+## Collections
+- `player_accounts`: This collection is responsible for storing 
+###### Schema:
--- src/server.js
+++ src/server.js
@@ -6,17 +6,25 @@ const app = express();
 const port = 3001;
 const api_path = '/api/v1';
 
-const mongoClient = new MongoClient(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const constructProperMongoURI = (env_var_name) => {
+  let uri =
+    'mongodb+srv://' + process.env[env_var_name] + '@' + process.env.MONGO_URI;
+
+  return uri;
+};
 
 app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+  console.log(constructProperMongoURI('MGDB_PLAYERMANAGER'));
+});
+
+app.get(api_path + '/hello', (req, res) => {
+  res.json({ status: 200, message: 'Hello world!' });
 });
 
 app.get(api_path + '/player/:id', (req, res) => {
+  console.log(req.params.id);
   res.json({ status: 200, message: 'You are pinging the players endpoint' });
 });
