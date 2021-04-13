98a907e340b5653aad6f2cfa5c8835126605fff7
sean-hale-dev <hales3@rpi.edu>
2021-04-12 21:04:29 -0400

    Functional map display for create screen as the MapViewCard component

--- src/frontend/src/components/Create.js
+++ src/frontend/src/components/Create.js
@@ -4,6 +4,7 @@ import Leaflet, { Icon } from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { RollBoxLoading } from 'react-loadingg';
 
 // Import MaterialUI elements
 import {
@@ -36,8 +37,16 @@ import AddBoxIcon from '@material-ui/icons/AddBox';
 import '@assets/style/create.scss';
 import Header from '@components/shared/Header';
 import MapSelect from '@components/shared/MapSelect';
+import API from '@root/src/api';
+import DefaultMarker from '@assets/img/mapPinDefault.svg';
 
 delete Leaflet.Icon.Default.prototype._getIconUrl;
+
+Leaflet.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 const StartCreateNewMap = forwardRef((props, ref) => {
   const [open, setOpen] = useState(false);
@@ -138,13 +147,62 @@ class MapViewCard extends Component {
     this.state = {
       mapID: props.mapID,
       mapObj: null,
+      loading: true,
     };
+  }
+
+  async componentDidMount() {
+    let mapData = await API.get(`/api/v1/maps/${this.state.mapID}`);
+    mapData = mapData.data;
+    this.setState({ loading: false, mapObj: mapData });
+    let mapCenter = [0, 0];
+    this.state.mapObj.pointsOfInterest.map((point) => {
+      mapCenter[0] += point.lat;
+      mapCenter[1] += point.long;
+    });
+    mapCenter[0] /= this.state.mapObj.pointsOfInterest.length;
+    mapCenter[1] /= this.state.mapObj.pointsOfInterest.length;
+
+    this.setState({ mapCenter: mapCenter });
+
+    console.log(this.state);
   }
 
   render() {
     return (
       <>
-        <h1>{this.state.mapID}</h1>
+        <Box width="50%" minWidth="200px" height="100%" minHeight="200px">
+          {this.state.loading ? (
+            <RollBoxLoading speed={250} color="#acacac" />
+          ) : (
+            <MapContainer
+              center={[42.72983440371727, -73.68997137045602]}
+              zoom={16}
+            >
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+              />
+              {this.state.mapObj.pointsOfInterest.map((poi) => {
+                return (
+                  <Marker
+                    key={poi.name}
+                    position={[poi.lat, poi.long]}
+                    icon={
+                      new Icon({
+                        iconUrl: DefaultMarker,
+                        iconSize: [25, 41],
+                        iconAnchor: [12, 41],
+                      })
+                    }
+                  >
+                    <Popup>{poi.name}</Popup>
+                  </Marker>
+                );
+              })}
+            </MapContainer>
+          )}
+        </Box>
       </>
     );
   }
@@ -393,7 +451,7 @@ class Create extends Component {
   render() {
     return (
       <>
-        <MapViewCard mapID="this is my id" />
+        <MapViewCard mapID="6074d236155b07b430f47acf" />
       </>
     );
   }
